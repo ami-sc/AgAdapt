1f19c3a7ca96d7451dd8a43bf6ddf39acfc7fee0
ami-sc <github@mail.ami.sc>
2021-12-26 00:09:02 -0600

    Add VCF to CSV conversion script.

---
+++ Code/Utilities/VCF_To_CSV_File_Conversion.R
@@ -0,0 +1,43 @@
+# ******************************************************************************
+# **** AgAdapt Project ****
+#
+# - File:        VCF_To_CSV_File_Conversion.R
+#
+# - Description: Simple script to convert a .vcf file containing Genotype Data
+#                into a .csv file.
+#
+# - Required Libraries:
+#   - adegenet
+#   - vcfR
+# ******************************************************************************
+
+library(adegenet)
+library(vcfR)
+
+#'
+#' .vcf To .csv File Conversion
+#'
+#' Extracts Genotype Data from a .vcf file and writes it to a .csv file.
+#'
+#' @param   vcf_file   String - Path of .vcf file to extract data from.
+#' @param   csv_file   String - Path of .csv file to write data to.
+#'
+vcf_to_csv <- function(vcf_file, csv_file)
+{
+  cat("\n\n[ > ] Extracting genotype data from .vcf file.\n\n")
+  genotype_data <- read.vcfR(vcf_file)
+
+  cat("\n\n[ > ] Loading genotype data into a genlight object.\n\n")
+  genotype_data <- vcfR2genlight(genotype_data)
+
+  cat("\n\n[ > ] Summary of genotype data.\n\n")
+  print(head(genotype_data))
+
+  cat("\n\n[ > ] Converting genlight object into matrix.\n\n")
+  genotype_data <- as.matrix(genotype_data)
+
+  cat("\n\n[ > ] Saving matrix to .csv file.\n\n")
+  write.table(genotype_data, file = csv_file, col.names = NA, sep = ",")
+
+  cat("\n\n[ > ] Done!\n\n")
+}
